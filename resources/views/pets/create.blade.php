0c6940854043a719bf541ad295a776a2f4ebca4a
KacperB <kacpero.barszczewski@gmail.com>
2025-01-19 20:30:37 +0100

    style

--- resources/views/pets/create.blade.php
+++ resources/views/pets/create.blade.php
@@ -56,7 +56,7 @@
 
     <div>
         <button type="submit"
-            class="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+            class="w-full bg-blue-500 text-white font-bold mb-4 py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
             Dodaj
         </button>
     </div>
