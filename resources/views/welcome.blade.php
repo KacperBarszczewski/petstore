5d655bf5aad0fb89e3b6141fb32d4d92446730c5
KacperB <kacpero.barszczewski@gmail.com>
2025-01-19 14:32:12 +0100

    style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,52 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>Laravel</title>
 
-        <!-- Styles / Scripts -->
-        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @else
-            <style>
-                
-            </style>
-        @endif
-    </head>
-    <body class="font-sans antialiased bg-gray-100">
-        <div class="min-h-screen flex flex-col items-center justify-center">
-            <h1 class="text-4xl font-bold text-gray-800">Welcome to Laravel</h1>
-            <p class="text-lg text-gray-600 mt-4">This is a Laravel application with Vite.js</p>
-        </div>
-        
-    </body>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+        <style>
+
+        </style>
+    @endif
+</head>
+
+<body class="font-sans antialiased bg-gray-100 text-gray-600 min-h-screen flex flex-col">
+    <header>
+        <nav
+            class="flex flex-row p-4 bg-gray-200 font-semibold  items-center justify-center  *:basis-1/3 *:flex *:items-center">
+            <div class="justify-start">
+                <a href="">Lista</a>
+            </div>
+            <div class="text-lg font-bold justify-center">
+                <a href="https://github.com/KacperBarszczewski" target="_blank" rel="noopener noreferrer">Kacper Barszczewski</a>
+            </div>
+            <div class="justify-end">
+                <a href="">Dodaj zwierzaka</a>
+            </div>
+
+        </nav>
+    </header>
+
+    <div class="flex-grow flex flex-col items-center justify-center">
+        <h1 class="text-4xl font-bold text-gray-800">Welcome to Laravel</h1>
+        <p class="text-lg mt-4">This is a Laravel application with Vite.js</p>
+    </div>
+
+    <footer class="p-4 bg-gray-200 font-semibold text-center">
+        <a href="https://github.com/KacperBarszczewski" target="_blank" rel="noopener noreferrer">&copy; Kacper Barszczewski</a>
+    </footer>
+
+</body>
+
 </html>
